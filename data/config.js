5839cca2a37ebd61244080465d3c785f8e8c95f9
Kemberly Eliscar <kemberlyeliscar@gmail.com>
2020-03-23 17:33:54 -0400

    set environment variable

--- data/config.js
+++ data/config.js
@@ -1,6 +1,8 @@
 const knex = require("knex")
 const knexfile = require("../knexfile")
 
+// we are checking our environment from NODE_ENV and we are defaulting to the environment of 'dev' 
 const environment = process.env.NODE_ENV || "dev"
 
+// and we are using that to pull in the database config from the knex file
 module.exports = knex(knexfile[environment])
